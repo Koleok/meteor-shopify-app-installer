a620a6e2cd42c317c0c2866a1c8e7de4ac88b1de
Kyle Chamberlain (MacbookPro) <kchamberlain@airwavesinc.com>
2015-09-16 10:09:42 -0400

    Update git ref in package.js

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'A simple package to manually install a shopify app from outside shopify app store.',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://bitbucket.org/airwaves/meteor-packages',
+  git: 'https://github.com/Koleok/meteor-shopify-app-installer.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
